a8562b3cd2083af0a06a6a094cae2d1d582959ac
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-14 16:31:26 -0700

    Resetting Project

--- convex/schema.ts
+++
@@ -1,6 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  homepage: defineTable({ message: v.string() }),
-});
