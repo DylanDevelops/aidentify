b02249a5e79683182940d407c7ef66de9fbc9a9c
Divi Newton <divinewton@gmail.com>
2024-10-15 08:57:54 -0700

    Added dark mode logos

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,7 +6,8 @@ import Image from "next/image";
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <Image className="mx-auto" src="/Wordmark_Logo.svg" width={500} height={500} alt="AIdentify Logo" />
+      <Image className="mx-auto dark:hidden" src="/logos/Wordmark_Logo.svg" width={500} height={500} alt="AIdentify Logo" />
+      <Image className="mx-auto hidden dark:block" src="/logos/Wordmark_Logo-darkmode.svg" width={500} height={500} alt="AIdentify Logo" />
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Thanks for checking out the website!
       </h3>
