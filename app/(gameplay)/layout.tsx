c381dc26b6b983191ebbb93beba2ee19c65804b2
Dylan Ravel <dylanm.ravel@gmail.com>
2024-10-31 20:38:53 -0700

    Hopefully fixed deployment issue

--- app/(gameplay)/layout.tsx
+++ app/(gameplay)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/navbar";
+import { Suspense } from "react";
 
 const GameplayLayout = ({
   children
@@ -11,7 +12,9 @@ const GameplayLayout = ({
         <Navbar />
         {/* TODO: bg need to account for dark mode */}
         <main className="h-full pt-40 bg-[radial-gradient(50%_50%_at_50%_50%,_#B7CECE_0%,_#E2E2E2_100%)]">
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>
+            {children}
+          </Suspense>
         </main>
       </div> 
     </>
