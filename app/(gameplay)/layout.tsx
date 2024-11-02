247280d4208616784b6cc2763b8613855a5dd127
Dylan Ravel <dylanm.ravel@gmail.com>
2024-11-01 18:11:56 -0700

    Added responsive loading

--- app/(gameplay)/layout.tsx
+++ app/(gameplay)/layout.tsx
@@ -1,4 +1,6 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
+import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
 
 const GameplayLayout = ({
@@ -12,7 +14,16 @@ const GameplayLayout = ({
         <Navbar />
         {/* TODO: bg need to account for dark mode */}
         <main className="h-full pt-40 bg-[radial-gradient(50%_50%_at_50%_50%,_#B7CECE_0%,_#E2E2E2_100%)]">
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={(
+            <>
+              <div className="min-h-full flex flex-col">
+                <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+                  <Loader2 className="w-32 h-32 animate-spin" />
+                </div>
+              </div>
+              <Footer />
+            </>
+          )}>
             {children}
           </Suspense>
         </main>
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,7 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 const MarketingLayout = ({
   children
@@ -10,7 +13,18 @@ const MarketingLayout = ({
       <div className="h-full">
         <Navbar />
         <main className="h-full pt-40">
-          {children}
+          <Suspense fallback={(
+            <>
+              <div className="min-h-full flex flex-col">
+                <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+                  <Loader2 className="w-32 h-32 animate-spin" />
+                </div>
+              </div>
+              <Footer />
+            </>
+          )}>
+            {children}
+          </Suspense>
         </main>
       </div> 
     </>
