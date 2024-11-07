f841778672d7b42950811b5cee1689b3cc0175b7
Dylan Ravel <dylanm.ravel@gmail.com>
2024-11-07 13:07:55 -0800

    Added popup alert for gamemode coming soon

--- app/(gameplay)/game/_components/TextGame.tsx
+++ app/(gameplay)/game/_components/TextGame.tsx
@@ -8,14 +8,14 @@ const TextGame = () => {
         <p className="text-[1.125rem]">Select the AI Generated Text Sample</p>
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center text-center gap-10 md:gap-[6.25rem] flex-1 px-6 py-10 md:py-2">
-        <div className="w-[25rem] h-[25rem] rounded-[2.5rem] hover:border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300 flex items-center justify-center text-center p-10">
+        <div className="w-[25rem] h-[25rem] rounded-[2.5rem] hover:border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300 flex items-center justify-center text-center p-10" onClick={() => alert("Text mode coming soon!")}>
           <div>
             <h1 className="font-bold pb-3">Koi</h1>
             <p>Among heart-shaped leaves<br />the white fish gleams, red tail.<br />Soft lotuses sleep.</p>
           </div>
         </div>
         <div className="w-[25rem] h-[3px] md:w-[3px] md:h-[25rem] bg-[rgba(110,_126,_133)] rounded"></div>
-        <div className="w-[25rem] h-[25rem] rounded-[2.5rem] hover:border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300 flex items-center justify-center text-center p-10">
+        <div className="w-[25rem] h-[25rem] rounded-[2.5rem] hover:border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300 flex items-center justify-center text-center p-10" onClick={() => alert("Text mode coming soon!")}>
           <div>
             <h1 className="font-bold pb-3">Nature&apos;s Embrace</h1>
             <p>Whispers in the breeze,<br />Sunlight dances on the leaves,<br />Nature&apos;s soft embrace.</p>
