df892aa132767a8fc945920b18878334edb23a16
Dylan Ravel <dylanm.ravel@gmail.com>
2024-11-06 22:36:46 -0800

    Some loading stuff

--- app/(gameplay)/game/_components/ImageGame.tsx
+++ app/(gameplay)/game/_components/ImageGame.tsx
@@ -1,5 +1,5 @@
 import { Footer } from "@/components/footer";
-import { TriangleAlert } from "lucide-react";
+import { Loader2, TriangleAlert } from "lucide-react";
 import Image from "next/image";
 import { useGame } from "../_context/GameContext";
 import { Id } from "@/convex/_generated/dataModel";
@@ -35,19 +35,27 @@ const ImageGame = () => {
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center text-center gap-10 md:gap-[6.25rem] flex-1 px-6 py-10 md:py-2">
         <div className="w-[25rem] h-[25rem] rounded-[2.5rem] border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300" onClick={() => {
+          if(isLoading) return;
+
           handleSubmittingGuess(currentImageIds![0]);
         }}>
           { (isLoading) ? (
-            <Skeleton className="w-full h-full" />
+            <div className="flex items-center justify-center w-full h-full">
+              <Loader2 className="w-1/2 h-1/2 animate-spin" />
+            </div>
           ) : (
             <Image src={currentImageSrcUrls[0]} layout="fill" objectFit="cover" draggable={false} alt="Image Option" className="absolute inset-0 w-full h-full object-cover" />
           )}
         </div>
         <div className="w-[25rem] h-[25rem] rounded-[2.5rem] border-[5px] border-[rgba(110,_126,_133)] overflow-hidden relative select-none sm:hover:scale-105 cursor-pointer transition-transform duration-300" onClick={() => {
+          if(isLoading) return;
+
           handleSubmittingGuess(currentImageIds![1]);
         }}>
           { (isLoading) ? (
-            <Skeleton className="w-full h-full" />
+            <div className="flex items-center justify-center w-full h-full">
+              <Loader2 className="w-1/2 h-1/2 animate-spin" />
+            </div>
           ) : (
             <Image src={currentImageSrcUrls[1]} layout="fill" objectFit="cover" draggable={false} alt="Image Option" className="absolute inset-0 w-full h-full object-cover" />
           )}
